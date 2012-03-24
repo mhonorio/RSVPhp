ef5f51b06adb3f188b8374aaf1ad87c890b3c442
Marcio Seiji <marcio.seiji@gmail.com>
2012-03-24 11:08:27 -0300

    Ajuste das fontes

--- app/Controller/GuestsController.php
+++ app/Controller/GuestsController.php
@@ -8,30 +8,30 @@ class GuestsController extends AppController {
 	
 	public function confirm($hash)
 	{
-            $guest = $this->Guest->find('first', array(
-                'conditions' => array('hash' => $hash),
-                'recursive' => -1
-            ));
+		$guest = $this->Guest->find('first', array(
+			'conditions' => array('hash' => $hash),
+			'recursive' => -1
+		));
 
-            $to = 'marcelohonorio@gmail.com';
-            $subject = "[RSVPHP] Confirmação {$guest['Guest']['first_name']} {$guest['Guest']['last_name']}";
-            $body = "Confirmação de presença de {$guest['Guest']['first_name']} {$guest['Guest']['last_name']}. Companhias: {$guest['Guest']['companions']}";
+		$to = 'marcelohonorio@gmail.com';
+		$subject = "[RSVPHP] Confirmação {$guest['Guest']['first_name']} {$guest['Guest']['last_name']}";
+		$body = "Confirmação de presença de {$guest['Guest']['first_name']} {$guest['Guest']['last_name']}. Companhias: {$guest['Guest']['companions']}";
 
-            mail($to, $subject, $body);
-            
-            $this->Guest->updateAll(
-                    array(
-                            'Guest.confirmed' => true,
-                            'Guest.modified' => 'NOW()'
-                    ),
-                    array(
-                            'Guest.hash' => $hash
-                    )
-            );
+		mail($to, $subject, $body);
 
-            $event = $this->Event->find('first');
+		$this->Guest->updateAll(
+			array(
+				'Guest.confirmed' => true,
+				'Guest.modified' => 'NOW()'
+			),
+			array(
+				'Guest.hash' => $hash
+			)
+		);
 
-            $this->set(compact('event'));
+		$event = $this->Event->find('first');
+
+		$this->set(compact('event'));
 	}
 	
 }
